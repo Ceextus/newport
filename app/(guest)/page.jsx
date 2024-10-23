f56e8405801428819a6e46364804ac2b60b9bf57
Sixtus Ojochegbe Joseph <sixtusjoseph3@gmail.com>
2024-10-23 22:42:15 +0100

    firsttt commit

--- app/(guest)/page.jsx
+++ app/(guest)/page.jsx
@@ -1,13 +1,23 @@
 "use client";
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { motion, useInView } from 'framer-motion';
 import Typewriter from 'react-typewriter-effect';
 import Contact from '../componets/Contact';
 import Recent from '../componets/Recent';
+
 const HomePage = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Set to true to confirm the component is mounted on the client-side
+    setIsClient(true);
+  }, []);
+
   const handleResumeDownload = () => {
     const resumeUrl = './sixtus-Resume.pdf'; 
-    window.open(resumeUrl, '_blank');
+    if (typeof window !== 'undefined') {
+      window.open(resumeUrl, '_blank');
+    }
   };
 
   return (
